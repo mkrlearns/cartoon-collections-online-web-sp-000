75731dfb7bd108f2ec06451de08894696c371efe
ryan m <contact@ryanmeek.com>
2019-03-06 19:13:43 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,7 +1,8 @@
 # ["Doc", "Dopey", "Bashful", "Grumpy"]
 
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(names)
+  names.each_with_index do |name|
+    puts 
 end
 
 def summon_captain_planet# code an argument here
