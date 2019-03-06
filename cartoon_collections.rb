a9934dc9ac5bb0c524da9cea497f0eada31c1637
ryan m <contact@ryanmeek.com>
2019-03-06 19:34:42 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -16,7 +16,6 @@ def find_the_cheese(ingredients)
   cheese_types = ["cheddar", "gouda", "camembert"]
   cheese_types.each do |cheese|
       return cheese if ingredients.include?(cheese)
-    end
   end
   return nil
 end
